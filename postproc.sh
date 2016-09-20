5272f19a50c6010d54f5b0516bf0e0fcdc0b640e
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-09-21 09:42:37 +1000

    Changes in 'Muru Mittigar email no. 2'

---
+++ postproc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd module
+
+echo "Button_Take_After_Photograph=Take Before Photograph" >> english.0.properties
+echo "Button_Take_Before_Photograph=Take After Photograph" >> english.0.properties
