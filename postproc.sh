8fd43f44445b8edb9be77ab9c4880306145a3bbb
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-09-22 10:14:46 +1000

    Latest changes

--- postproc.sh
+++ postproc.sh
@@ -2,5 +2,5 @@
 
 cd module
 
-echo "Button_Take_After_Photograph=Take Before Photograph" >> english.0.properties
-echo "Button_Take_Before_Photograph=Take After Photograph" >> english.0.properties
+echo "Button_Photo_Before=Photo Before" >> english.0.properties
+echo "Button_Photo_After=Photo After"   >> english.0.properties
